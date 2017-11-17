d38451a0fe1c1c4d077898a760333f3eef2fbe74
Brian Fan <kuomu.fan@yahoo.com>
2017-11-17 10:52:58 -0800

    production mode

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import './assets/scss/app.scss';
-import './assets/js/google_map';
-import './assets/js/portfolio';
-import 'jquery';
-import 'bootstrap'; // importing bootstrap.js
-
-import 'bootstrap/scss/bootstrap.scss'; // bootstrap.scss
-require('./assets/media/favicon.ico');
